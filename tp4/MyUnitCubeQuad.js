919055c19d37b688845bc1d50d1f0b0eca0bd3ec
PBeGood4 <pedrocbeirao@gmail.com>
2024-03-13 10:14:05 +0000

    fix cube face orientation

--- tp4/MyUnitCubeQuad.js
+++ tp4/MyUnitCubeQuad.js
@@ -92,7 +92,7 @@ export class MyUnitCubeQuad extends CGFobject {
     this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST)
     this.scene.pushMatrix()
     this.scene.translate(0,0,-0.5)
-    this.scene.rotate(Math.PI,1, 0, 0)
+    this.scene.rotate(Math.PI,0, 1, 0)
     this.quad.display()
     this.scene.popMatrix()
 
