cca10ddcf8afa2328e0bda7f609ce0ef607e4a25
AndreSantos03 <andrebernardosantos@gmail.com>
2024-04-23 16:56:51 +0100

    Finished Flower with leaves and parameters

--- project/objects/MyFlower.js
+++ project/objects/MyFlower.js
@@ -4,24 +4,31 @@ import { MyTriangle } from "./MyTriangle.js";
 import { MyCylinder } from "./MyCylinder.js";
 
 export class MyFlower extends CGFobject{
-    constructor(scene,flowerRadius=0.8,numberFlorets=6,floretsColor = '#FFFFFF',diskRadius=0.8,diskColor = '#FFFF00',stemRadius = 1,stemSize=4,stemColor='#4eb300'){
+    constructor(scene,flowerRadius=0.8,numberFlorets=6,floretsColor = '#FFFFFF',diskRadius=0.8,diskColor = '#FFFF00',stemRadius = 1,stemSize=3,stemColor='#4eb300',leavesColor='#afbd22'){
         super(scene);
+
+
+        
         this.stems = [];
         this.stemsAngles = [];
-        var maxStemAngle = Math.PI / 8;
+        var maxStemAngle = Math.PI / 40;
         for(let i = 0; i < stemSize; i++){
-            let stem = [new MyCylinder(scene,10,10)];
+            let stem = new MyCylinder(scene,10,10);
             this.stems.push(stem);
             this.stemsAngles.push([Math.random() * maxStemAngle]);
+            console.log(this.stemsAngles[i]);
         }
+
+        //leaves for the intersection of the stems
+        this.leave = new MyTriangle(scene);
+
         this.disk = new MySphere(scene,diskRadius,10,10);
+
         this.florets = [];
         for(let i = 0; i < 6; i++){
             let floret = [new MyTriangle(scene),new MyTriangle(scene)];
             this.florets.push(floret);
         }
-        
-
 
         this.flowerRadius = flowerRadius;
         this.numberFlorets = numberFlorets;
@@ -31,16 +38,13 @@ export class MyFlower extends CGFobject{
         this.stemRadius = stemRadius;
         this.stemSize = stemSize;
         this.stemColor = hexToRgb(stemColor);
+        this.leavesColor = hexToRgb(leavesColor);
 
 
         this.initMaterials();
     }
 
     initMaterials() {
-        // Stem color (green)
-        const stemColor = [0.1, 0.5, 0.1, 1];
-
- 
 
         // Stem material
         this.stemMaterial = new CGFappearance(this.scene);
@@ -63,6 +67,13 @@ export class MyFlower extends CGFobject{
         this.floretMaterial.setDiffuse(...this.floretsColor);
         this.floretMaterial.setSpecular(0.1, 0.1, 0.1, 1);
         this.floretMaterial.setShininess(10);
+
+        //leaves
+        this.leaveMaterial = new CGFappearance(this.scene);
+        this.leaveMaterial.setAmbient(...this.leavesColor.map(c => c * 0.2));
+        this.leaveMaterial.setDiffuse(...this.leavesColor);
+        this.leaveMaterial.setSpecular(0.1, 0.1, 0.1, 1);
+        this.leaveMaterial.setShininess(10);
     }
 
     display(){
@@ -70,18 +81,38 @@ export class MyFlower extends CGFobject{
     }
 
     draw(){
+
+        var stemLength = this.stemSize;
         //Stem
-        this.scene.pushMatrix();
-        this.scene.translate(0,this.stemSize * 4,0);
-        this.scene.rotate(Math.PI/2,1,0,0);
-        this.scene.scale(this.stemRadius * 0.2,this.stemRadius * 0.2,this.stemSize *4);
-        this.stemMaterial.apply();
-        this.stem.display();
-        this.scene.popMatrix();
+        for (let i = 0; i < this.stems.length; i++) {
+            var stem = this.stems[i];
+            var angle = this.stemsAngles[i];
+            this.scene.pushMatrix();
+            this.scene.translate(0,1 + i,0);
+            this.scene.rotate(angle,0,0,1);
+            this.scene.rotate(Math.PI/2,1,0,0);
+            this.scene.scale(this.stemRadius * 0.2,this.stemRadius * 0.2,1.1);
+            this.stemMaterial.apply();
+            stem.display();
+            this.scene.popMatrix();
+
+            //draw the leave for each intersection
+            if(i + 1 != this.stems.length){
+                this.scene.pushMatrix();
+                let aroundCaleAngle = (i % 2 === 0) ? Math.PI / 4 : Math.PI * 5 / 4; // so it switches side in intersection
+                this.scene.rotate(aroundCaleAngle,0,1,0);
+                this.scene.rotate(Math.PI/2,1,0,0);//rotate so its parralel to xz
+                this.scene.scale(0.4,0.4,1);
+                this.scene.translate(1,1,-1 -i);
+                this.leaveMaterial.apply();
+                this.leave.display();
+                this.scene.popMatrix();
+            }
+        }
 
         //Disk
         this.scene.pushMatrix();
-        this.scene.translate(0,4*this.stemSize,0.5);
+        this.scene.translate(0,stemLength,0.5);
         this.scene.scale(1,1,1);
         this.diskMaterial.apply();
         this.disk.display();
@@ -96,7 +127,7 @@ export class MyFlower extends CGFobject{
 
             // First Petal
             this.scene.pushMatrix();  
-            this.scene.translate(0,this.stemSize*4,0.5);
+            this.scene.translate(0,stemLength,0.5);
             this.scene.rotate(angle, 0, 0, 1); // rotate to be around the flower
             this.scene.scale(this.flowerRadius/4,this.flowerRadius, 1); // Scale flower
             this.scene.rotate(5 * Math.PI / 4, 0, 0, 1); // Rotate it upwards
@@ -108,7 +139,7 @@ export class MyFlower extends CGFobject{
 
             // // //Second Petal
             this.scene.pushMatrix();
-            this.scene.translate(0,this.stemSize * 4,0.5);
+            this.scene.translate(0,stemLength,0.5);
             this.scene.rotate(angle,0,0,1);
             this.scene.scale(this.flowerRadius/4,this.flowerRadius,1); //scale flower
             this.scene.rotate(Math.PI/4,0,0,1);//rotate it downwards
